448e27dce21a61ada2a6cdd6ef57d9f1df442c82
nevdiaz <jenndiazz@icloud.com>
2019-05-20 14:45:35 -0600

    Initial commit of args echo program

---
+++ src/edu/cnm/deepdive/Echo.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive;
+
+public class Echo {
+
+  public static void main(String[] args) {
+
+    for (int i=0; i < args.length; i++ ){
+      System.out.printf("%d: %s%n ", i, args[i].toUpperCase());
+
+    }
+  }
+
+}
